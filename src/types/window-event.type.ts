b4dec67205d18f1757d67bb99f3e09292bdc33d1
donneko <117331095+donneko@users.noreply.github.com>
2026-08-15 14:34:22 +0900

    fix: ファイル名のタイポ修正

--- src/types/window-event.type.ts
+++ src/types/window-event.type.ts
@@ -1,7 +1,7 @@
-type WindowEvents = {
+export type WindowEvents = {
     "window:move": { id: string; x: number; y: number };
     "window:close": { id: string };
-    "window:header-display-type": { id: string; type: TitleDisplayType };
+    "window:header-display-type": { id: string; type: DisplayType };
     "window:header-icon-valid": { id: string; flag: boolean };
     "window:header-icon-src": { id: string; src: string };
     "window:header-title-text": { id: string; value: string };
